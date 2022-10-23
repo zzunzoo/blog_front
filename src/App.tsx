3caad7910b81361c847798a9a36f3604a7e76adf
zzunzoo <zzunzoo@gmail.com>
2022-10-23 12:34:32 +0100

    blog_front

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="mt-5 text-center">
+      <div className="text-3xl">이나현 바보</div>
+    </div>
+  );
+}
+
+export default App;
